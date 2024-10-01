a6e7c67fd5fb1609fc9b2866c65474037049a095
startuip <lhw981117@gmail.com>
2024-10-01 17:00:20 +0800

    Update pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -93,8 +93,6 @@ fi
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-# Restart your shell for the changes to take effect.
-# Load pyenv-virtualenv automatically by adding
-# the following to ~/.bashrc:
 eval "$(pyenv virtualenv-init -)"
+sleep 5
 reboot
